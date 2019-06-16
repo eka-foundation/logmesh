99b3dc5b155228d1006951719b831df846151281
Mikko Kotila <mailme@mikkokotila.com>
2019-06-16 21:01:14 +0300

    Create one_prepare.py

---
+++ one_prepare.py
@@ -0,0 +1,42 @@
+import os
+import gzip
+
+# note that you should have batman.zip in pwd
+
+os.mkdir('mesh_logs')
+os.system('unzip batman.zip')
+os.system('mv batman/* mesh_logs')
+os.system('rm -r batman')
+
+# get the list of the nodes
+nodes = os.popen('ls mesh_logs').readlines()
+
+# convert the names to standard ip format
+for node in nodes:
+    
+    current = node.strip()
+    new = node.strip().replace('_', '.')
+
+    os.system('mv mesh_logs/' + current + ' mesh_logs/' + new)
+    
+# first we get the list of the node IPs
+for node in os.popen('ls mesh_logs').readlines():
+    
+    # remove newlines
+    node = node.strip()
+    
+    os.system('gzip -d mesh_logs/' + node + '/*.gz')
+    
+for node in os.popen('ls mesh_logs').readlines():
+    
+    # remove newlines
+    node = node.strip()
+    
+    os.system('cat mesh_logs/' + node + '/bat*.lo* > ' + node + '_routing.log')
+    os.system('cat mesh_logs/' + node + '/mesh*.lo* > ' + node + '_mesh.log')
+    os.system('cat mesh_logs/' + node + '/ping_stats*.lo* > ' + node + '_ping.log')
+    os.system('cat mesh_logs/' + node + '/wifi*.lo* > ' + node + '_wifi.log')
+    
+os.mkdir('cleaned_logs')
+os.system('mv *.log cleaned_logs')
+os.system('rm -rf mesh_logs/')
